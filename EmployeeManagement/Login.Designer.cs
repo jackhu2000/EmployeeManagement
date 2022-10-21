2230a330b9fafa6d497dde673b61560462f71ab3
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2022-10-22 12:19:37 +1300

    Add sign up button

--- EmployeeManagement/Login.Designer.cs
+++ EmployeeManagement/Login.Designer.cs
@@ -35,13 +35,14 @@ namespace EmployeeManagement
             this.lblUsername = new System.Windows.Forms.Label();
             this.lblPassword = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUsername
             // 
             this.txtUsername.Location = new System.Drawing.Point(380, 108);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(110, 25);
+            this.txtUsername.Size = new System.Drawing.Size(187, 25);
             this.txtUsername.TabIndex = 0;
             // 
             // txtPassword
@@ -49,14 +50,14 @@ namespace EmployeeManagement
             this.txtPassword.Location = new System.Drawing.Point(380, 153);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(110, 25);
+            this.txtPassword.Size = new System.Drawing.Size(187, 25);
             this.txtPassword.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(380, 211);
+            this.btnLogin.Location = new System.Drawing.Point(376, 205);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(110, 25);
+            this.btnLogin.Size = new System.Drawing.Size(102, 25);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "&Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -91,11 +92,22 @@ namespace EmployeeManagement
             this.label1.Text = "Employee Management System";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(484, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "&Sign Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(819, 323);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUsername);
@@ -104,6 +116,7 @@ namespace EmployeeManagement
             this.Controls.Add(this.txtUsername);
             this.Name = "Login";
             this.Text = "Login - Employee Management System";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,5 +130,6 @@ namespace EmployeeManagement
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
