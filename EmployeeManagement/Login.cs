1d9d9e95d14480f2a697e27e48078c9b79341fe3
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2022-10-26 10:56:48 +1300

    Revert "remove conflict form"
    This reverts commit 0c16c326f116e2e36c6050d903c0f41c0b7f4a32.

---
+++ EmployeeManagement/Login.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace EmployeeManagement
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string cs = @"server=sql6.freesqldatabase.com;port=3306;userid=sql6527607;password=t4Rtas3nkN;database=sql6527607";
+            using var con = new MySqlConnection(cs);
+            try
+            {
+                con.Open();
+                string sql = "SELECT username, password, role FROM account WHERE username='" + username + "' AND password='" + password + "'" ;
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                if (rdr.HasRows)
+                {
+                    rdr.Read();
+                    string priviledge = rdr[2].ToString();
+                    if (priviledge == "a")
+                    {
+                        Form1 adminForm = new Form1();
+                        adminForm.Show();
+                    }
+                    else
+                    {
+                        Form3 employeForm = new Form3();
+                        employeForm.Show();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Username and Password");
+                }
+                
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error:\n" + ex.ToString());
+            }
+            con.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
