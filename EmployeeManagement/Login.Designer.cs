399d88efb82071b78dcd2eca1601e001548de37b
eshahas <764701627@nzse.ac.nz>
2022-10-27 12:30:39 +1300

    add mm

--- EmployeeManagement/Login.Designer.cs
+++ EmployeeManagement/Login.Designer.cs
@@ -94,9 +94,9 @@ namespace EmployeeManagement
             this.label1.Location = new System.Drawing.Point(139, 99);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(392, 32);
+            this.label1.Size = new System.Drawing.Size(458, 32);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Employee Management Systemm";
+            this.label1.Text = "Employee Management Systemmmmm";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnSignUP
