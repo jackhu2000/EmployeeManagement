e362936d7f31995d6b84cca00d105adecfba107d
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2022-11-04 13:13:11 +1300

    1. add Update Password Button to employee form 2. add Password Update Form

---
+++ EmployeeManagement/PasswordUpdateForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace EmployeeManagement
+{
+    partial class frmPasswordUpdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOldPassword = new System.Windows.Forms.Label();
+            this.txtNewPassword = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.Label();
+            this.txtOldPassW = new System.Windows.Forms.TextBox();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.txtConfirmNewPass = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOldPassword
+            // 
+            this.txtOldPassword.AutoSize = true;
+            this.txtOldPassword.Location = new System.Drawing.Point(87, 50);
+            this.txtOldPassword.Name = "txtOldPassword";
+            this.txtOldPassword.Size = new System.Drawing.Size(89, 17);
+            this.txtOldPassword.TabIndex = 0;
+            this.txtOldPassword.Text = "Old Password";
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.AutoSize = true;
+            this.txtNewPassword.Location = new System.Drawing.Point(87, 100);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.Size = new System.Drawing.Size(94, 17);
+            this.txtNewPassword.TabIndex = 1;
+            this.txtNewPassword.Text = "New Password";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.AutoSize = true;
+            this.txtConfirmPassword.Location = new System.Drawing.Point(122, 148);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(54, 17);
+            this.txtConfirmPassword.TabIndex = 2;
+            this.txtConfirmPassword.Text = "Confirm";
+            // 
+            // txtOldPassW
+            // 
+            this.txtOldPassW.Location = new System.Drawing.Point(203, 47);
+            this.txtOldPassW.Name = "txtOldPassW";
+            this.txtOldPassW.Size = new System.Drawing.Size(187, 25);
+            this.txtOldPassW.TabIndex = 1;
+            this.txtOldPassW.UseSystemPasswordChar = true;
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.Location = new System.Drawing.Point(203, 92);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.Size = new System.Drawing.Size(187, 25);
+            this.txtNewPass.TabIndex = 2;
+            this.txtNewPass.UseSystemPasswordChar = true;
+            // 
+            // txtConfirmNewPass
+            // 
+            this.txtConfirmNewPass.Location = new System.Drawing.Point(203, 140);
+            this.txtConfirmNewPass.Name = "txtConfirmNewPass";
+            this.txtConfirmNewPass.Size = new System.Drawing.Size(187, 25);
+            this.txtConfirmNewPass.TabIndex = 3;
+            this.txtConfirmNewPass.UseSystemPasswordChar = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(203, 200);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(83, 25);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "&Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // frmPasswordUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 276);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtConfirmNewPass);
+            this.Controls.Add(this.txtNewPass);
+            this.Controls.Add(this.txtOldPassW);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtNewPassword);
+            this.Controls.Add(this.txtOldPassword);
+            this.Name = "frmPasswordUpdate";
+            this.Text = "Password Update Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtOldPassword;
+        private System.Windows.Forms.Label txtNewPassword;
+        private System.Windows.Forms.Label txtConfirmPassword;
+        private System.Windows.Forms.TextBox txtOldPassW;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.TextBox txtConfirmNewPass;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
